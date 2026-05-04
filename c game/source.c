bafd7fbdc085c4b21fe7fa0e7fbe774f73ba9ae6
free75 <hyunho2st@gmail.com>
2026-05-04 19:54:26 +0900

    (2026 5. 4) 팩맨 6일차

--- c game/source.c
+++ c game/source.c
@@ -9,14 +9,24 @@
 #define COIN 2
 
 #define WIDTH 20
-#define HEIGHT 11
+#define HEIGHT 12
 
 HANDLE aBuffer[2];
 int buffer = 0;
 
+enum Gmaestate {
+	MENU,
+	PLAYING,
+	CLEAR,
+	FAIL
+};
+
+int gameState = PLAYING;
+
+
 // pacman의 입과 몸을 C 와 O 로 표현하고 2프레임으로 구현할 명령어
-char Pacman[2] = {'C', 'O'};
-int animation = 0;
+char PacmanAnim[2] = {'C', 'O'};
+int anim = 0;
 
 // 오브젝트 위치 * (플레이어 위치, 고스트(방해몹) 위치)
 // 배열문 선언 후에 콘솔 창에 나올 배열문에 함수를 포함해서
@@ -28,9 +38,6 @@ int animation = 0;
 
 // 팩맨 에니메이션 추가 이후
 // 방향 관련 명령어 추가
-
-
-int PACK = 0;
 
 int px = 1;
 int py = 1;
@@ -63,14 +70,9 @@ void clear() {
 		GENERIC_READ | GENERIC_WRITE,
 		0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 
-	SetConsoleActiveScreenBuffer(aBuffer[0]);
 
 	// 콘솔 창의 커서를 숨기기 위해 사용한 명령어
-	CONSOLE_CURSOR_INFO cursor;
-
-	cursor.dwSize = 1;
-
-	cursor.bVisible = FALSE;
+	CONSOLE_CURSOR_INFO cursor = { 1, FALSE };
 
 	GetConsoleCursorInfo(aBuffer[0], &cursor);
 
@@ -80,7 +82,7 @@ void clear() {
 
 }
 
-void render(HANDLE Buffer, int x, int y, const char* character, int color) {
+void render(HANDLE Buffer, int x, int y, char character, int color) {
 
 	COORD position = { x, y };
 
@@ -90,34 +92,33 @@ void render(HANDLE Buffer, int x, int y, const char* character, int color) {
 
 	DWORD dword;
 
-	WriteFile(Buffer, character, strlen(character), &dword, NULL);
-
+	WriteConsole(Buffer, &character, 1, &dword, NULL);
 }
 
-void renderText(HANDLE Buffer, int x, int y, int score, int color) {
-	char character[2];
-	character[0] = score + '0';
-	character[1] = '\0';
-	render(Buffer, x, y, character, color);
-}
+//void renderText(HANDLE Buffer, int x, int y, int score, int color) {
+//	char character[2];
+//	character[0] = score + '0';
+//	character[1] = '\0';
+//	render(Buffer, x, y, character, color);
+//}
 
-void renderNumber(HANDLE Buffer, int x, int y, int num, int color) {
+void renderNumber(HANDLE Buffer, int x, int y, int num) {
 
 	if (num == 0) {
-		renderText(Buffer, x, y, 0, color);
+		render(Buffer, x, y, '0', 7);
 		return;
 	}
 
-	int scores[10];
-	int count = 0;
+	char temp[10];
+	int i = 0;
 
 	while (num > 0) {
-		scores[count++] = num % 10;
+		temp[i++] = (num % 10) + '0';
 		num /= 10;
 	}
 
-	for (int i = count - 1; i >= 0; i--) {
-		renderText(Buffer, x++, y, scores[i], color);
+	for (int j = i - 1; j >= 0; j--) {
+		render(Buffer, x++, y, temp[i], 7);
 	}
 }
 
@@ -131,18 +132,20 @@ int go(int x, int y) {
 }
 
 // player가 방향키(UP, DOWN, RIGHT, LEFT)에 따라 어떻게 움직여야하는지 적을 명령어
-void player(char key) {
+void player() {
     if (_kbhit()) {
+
 		int key = _getch();
-		int nx = px, ny = py;
+		int nx = px;
+		int ny = py;
 
 		if (key == 224) {
 			key = _getch();
 
-			if (key == 72) { ny--; PACK = 2; }
-			if (key == 75) { nx--; PACK = 1; }
-			if (key == 77) { nx++; PACK = 0; }
-			if (key == 80) { ny++; PACK = 3; }
+			if (key == 72) ny--;
+			if (key == 75) nx--; 
+			if (key == 77) nx++; 
+			if (key == 80) ny++; 
 		}
 		if (go(nx, ny)) {
 			px = nx;
@@ -153,112 +156,102 @@ void player(char key) {
 				score++;
 			}
 		}
+
+		// VK_SPACE 사용 후에 스테이지 넘어갈때 사용할 코드]
+		if (key == 32) {
+			if (gameState != PLAYING) {
+				gameState = PLAYING;
+				px = 1; py = 1;
+				gx = 10; gy = 5;
+				score = 0;
+				
+			}
+		}
 	}
 }
 
 void ghostmove() {
 
-	int ghost[4][2] = {            // 이전 방해몹 코드 보완 while문으로 대체함
-		{0,-1},{0,1},{-1,0},{1,0}  // 따로 분리하지 않고 while 반복문으로 통일
-		                           // while문 그리고 if문 사용
-	};                             // while문 사용시 추적 방해몹이 위 아래로만 움직이는 것을 확인함
-	                               // for문으로 대체
+	int dx[4] = { 1, -1, 0, 0 };
+	int dy[4] = { 0, 0, 1, -1 };
 
-	int MAP = 999999;
-	int GHOSTX = gx;
-	int GHOSTY = gy;
+	int memory = 9999;
+	int Memory = -1;
 
-	// 앞으로만 움직이는 거 방지차기 위해 만든 코드
-	for (int i = 0; i < 4; i++) { // For문을 하나 더 선언함
-		                          // 이유 방지를 위해서 continue와 함께 사용
-		                          // rand와 배열문으로 방지 가능함
-
-		int r = rand() % 4;
-
-		int tx = ghost[i][0];
-		int ty = ghost[i][1];
-
-		ghost[i][0] = ghost[r][0];
-		ghost[i][1] = ghost[r][1];
-		
-		ghost[r][0] = tx;
-		ghost[r][1] = ty;
-
-	}
 	for (int i = 0; i < 4; i++) {
-
-		int r = rand() % 4;
-
-		int nx = gx + ghost[i][0];
-		int ny = gy + ghost[i][1];
+		int nx = gx + dx[i];
+		int ny = gy + dy[i];
 
 		if (!go(nx, ny)) continue;
 
-		int dx = px - nx;
-		int dy = py - ny;
+		int GHOST = (px - nx) * (px - nx) + (py - ny) * (py - ny);
 
-		int Map = dx * dx + dy * dy;
 
-		if (Map < MAP) {
-			MAP = Map;
-			GHOSTX = nx;
-			GHOSTY = ny;
-
+		if (GHOST < memory) {
+			memory = GHOST;
+			Memory = i;
 		}
-
-		gx = GHOSTX;
-		gy = GHOSTY;
 	}
 
+	if (Memory != -1) {
+		gx += dx[Memory];
+		gy += dy[Memory];
+	}
+}
+
+// touch 코드의 명령어 방해몹과 플레이어가 붙었을 경우 실행되는 명령어
+int touch() {
+	if (px == gx && py == gy)
+		gameState = FAIL;
+
+	if (score >= 50)
+		gameState = CLEAR;
 }
 
 void initialize() {
+
 	HANDLE Buffer = aBuffer[buffer];
-	SetConsoleActiveScreenBuffer(Buffer);
+
+	DWORD dword;
+	COORD position = { 0, 0 };
+	FillConsoleOutputCharacter(Buffer, ' ', WIDTH * HEIGHT, position, &dword);
+	FillConsoleOutputAttribute(Buffer, 7, WIDTH * HEIGHT, position, &dword);
 
 	for (int y = 0; y < HEIGHT; y++) {
 		for (int x = 0; x < WIDTH; x++) {
 
 			if (map[y][x] == WALL) // if문 안에서 행렬을 설정할 때에는 설정한 배열문이 있다면 [y]를 y축을 시작으로, [x] x축으로 끝나야 정상 작동하게 됩니다.
-				render(Buffer, x, y, "#", 8);
+				render(Buffer, x, y, '#', 8);
 			else if (map[y][x] == COIN)
-				render(Buffer, x, y, ".", 10);
+				render(Buffer, x, y, '.', 10);
 			else
-				render(Buffer, x, y, " ", 7);
+				render(Buffer, x, y, ' ', 7);
 
 		}
 	}
 
-	char PACKCH;
-
-	// PACKMAN 플레이어의 UP, DOWN, LEFT, RIGHT 전부 각각 표현하기 위해 사용한 코드
-	if (animation == 0) {
-		PACKCH = 'O';
+	if (gameState == PLAYING) {
+		render(Buffer, px, py, PacmanAnim[anim], 14);
+		render(Buffer, gx, gy, 'G', 12);
 	} 
-	else {
-		if (PACK == 0) PACKCH = '>';
-		else if (PACK == 1) PACKCH = '<';
-		else if (PACK == 2) PACKCH = '^';
-		else PACKCH = 'V';
 
-	}
+	render(Buffer, 0, HEIGHT - 1, 'S', 7);
+	renderNumber(Buffer, 2, HEIGHT - 1, score);
+	
+    if (gameState == CLEAR) 
+		render(Buffer, 5, 5, 'C', 10);
+	
 
-	// 스코어 명령어 코드
-	char pm[2] = { Pacman[animation], '\0' }; 
+	if (gameState == FAIL) 
+		render(Buffer, 8, 5, 'F', 12);
+	
 
-	render(Buffer, px, py, pm, 14);
-	render(Buffer, gx, gy, "G", 12);
-
-	render(Buffer, 0, HEIGHT - 1, "Score", 7);
-	renderNumber(Buffer, 7, HEIGHT - 1, score, 7);
-
-	buffer = 1 - buffer; 
+	SetConsoleActiveScreenBuffer(Buffer);
+	Buffer = !Buffer;
 }
 
-// touch 코드의 명령어 방해몹과 플레이어가 붙었을 경우 실행되는 명령어
-int touch() {
-	return (px == gx && py == gy);
-}
+
+
 
 // 게임 승리 명령어 추가
 
@@ -277,26 +270,34 @@ int main()
 {
 	clear();
 
-	int delay = 0;
-
+	int ghostTimer = 0;
+	int PlayerTimer = 0;
+ 
 	while (1) {
-		player(1);
 
-		delay++;
+		player();
 
-		if (delay % 3 == 0)
-			ghostmove();
+		if (gameState == PLAYING) {
+			
+			ghostTimer++;
+			if (ghostTimer >= 3) {
+				ghostmove();
+				ghostTimer = 0;
+			}
 
-		if (delay % 2 == 0)
-			animation = 1 - animation;
+			PlayerTimer++;
+			if (PlayerTimer >= 5) {
+				anim = !anim;
+				PlayerTimer = 0;
+			}
+
+			touch();
+		}
 
 		initialize();
 
-		if (touch()) 
-			break;
-
 		Sleep(60);
-
+	
 	}
 
 	// 지금까지 적어놓은 코드를 계속 실행해보면서 플레이 해본 결과
